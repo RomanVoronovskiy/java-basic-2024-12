5a97b3f15f47475adb666f1d11e43dfc503abae3
Roman Voronovskii <121174759+RomanVoronovskiy@users.noreply.github.com>
2025-04-14 19:42:28 +0300

    update lesson

--- src/main/java/ru/otus/java/basic/lesson20/client/Client.java
+++ src/main/java/ru/otus/java/basic/lesson20/client/Client.java
@@ -7,22 +7,5 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while (true) {
-            try (Socket socket = new Socket("localhost", 8080)) {
-                ExampleClient client = new ExampleClient(socket.getInputStream(),
-                        socket.getOutputStream());
-                System.out.println("Введи сообщение : ");
-                String userMessage = scanner.nextLine();
-                if (userMessage.equalsIgnoreCase("exit")) {
-                    client.send(userMessage);
-                    break;
-                }
-                client.send(userMessage);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
+
 }
--- src/main/java/ru/otus/java/basic/lesson20/server/Server.java
+++ src/main/java/ru/otus/java/basic/lesson20/server/Server.java
@@ -8,30 +8,5 @@ import java.net.Socket;
 
 public class Server {
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket socket = new ServerSocket(8080);
-        System.out.println("SERVER START!");
-        while (true) {
-            Socket client = socket.accept();
-            DataInputStream inputStream = new DataInputStream(client.getInputStream());
-            DataOutputStream outputStream = new DataOutputStream(client.getOutputStream());
-            System.out.println("client.getPort() = " + client.getPort());
-            String userInput = inputStream.readUTF();
-            System.out.println("получили от пользователя : " + userInput);
-            if (userInput.equalsIgnoreCase("exit")) {
-                System.out.println("Клиент с портом :" + client.getPort() + " отключился!");
-                client.close();
-                continue;
-            }
-            String result = transformStringToUpperCase(userInput);
-            outputStream.writeUTF(result);
-            outputStream.flush();
-            System.out.println("result после трансформации клиенту = " + result);
-        }
-    }
 
-    private static String transformStringToUpperCase(String userInput) {
-        System.out.println("Выполняем трансформацию!");
-        return userInput.toUpperCase();
-    }
 }
